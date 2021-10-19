3adbe77b0a4209c46db10d20b8cbd32590ed46ba
Davidsd16 <davidsanchezdiaz72@gmail.com>
2021-10-19 19:59:58 +0200

    insert coordinates and movement of the horse following the mouse

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -4,15 +4,44 @@ GamePlayManager = {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
+
+        this.flagFirsMouseDown = false;
     },
     preload: function() {
         game.load.image('background', 'assets/images/background.png');
+        game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2);
     },
     create: function() {
         game.add.sprite(0, 0, 'background');
+        this.horse = game.add.sprite(0, 0, 'horse');
+        this.horse.frame = 0;
+        this.horse.x = game.width/2;
+        this.horse.y = game.height/2;
+        this.horse.anchor.setTo(0.5);
+
+        game.input.onDown.add(this.onTap, this);
+    },
+    onTap:function(){
+        this.flagFirsMouseDown = true;
     },
     update: function() {
+        if(this.flagFirsMouseDown){
+            var pointerX = game.input.x;
+        var pointerY = game.input.y;
 
+        var distX = pointerX - this.horse.x;
+        var distY = pointerY - this.horse.y;
+
+        if(distX>0){
+            this.horse.scale.setTo(1,1);
+        }else{
+            this.horse.scale.setTo(-1,1);
+        }
+
+        this.horse.x +=distX * 0.02;
+        this.horse.y +=distY * 0.02;
+        }
+        
     }
 }
 
